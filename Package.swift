96a80dc744921d2c321609d863186647503ed341
TomohiroNozawa <67447557+TomohiroNozawa@users.noreply.github.com>
2020-10-07 16:56:30 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,5 +3,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "OpalImagePicker"
+    name: "OpalImagePicker",
+    products: [
+        .library(
+            name: "OpalImagePicker",
+            targets: ["OpalImagePicker"]),
+    ],
+    targets: [
+        .target(
+            name: "OpalImagePicker",
+            dependencies: []),
+       ]
 )
