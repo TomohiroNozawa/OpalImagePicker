8a6f1d66d00d83d601fd2c980d577ec746685bb1
TomohiroNozawa <67447557+TomohiroNozawa@users.noreply.github.com>
2020-10-07 17:15:23 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "OpalImagePicker",
+    defaultLocalization: "en",
     products: [
         .library(
             name: "OpalImagePicker",
